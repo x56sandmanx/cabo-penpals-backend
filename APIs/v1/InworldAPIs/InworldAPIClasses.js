f2f67be490c6e10081e07bb23f3c5b153d257803
Colin Sandman <xx56sandmanxx@gmail.com>
2024-12-18 14:04:46 -0500

    finalize

--- APIs/v1/InworldAPIs/InworldAPIClasses.js
+++ APIs/v1/InworldAPIs/InworldAPIClasses.js
@@ -5,3 +5,93 @@ export class InworldCharacter {
     this.commonKnowledge = inworldCharacter.commonKnowledge ? [...inworldCharacter.commonKnowledge] : []
   }
 }
+
+export class InworldKnowledge {
+  name
+  knowledgeRecords
+
+  constructor(inworldKnowledge = {}) {
+    this.name = inworldKnowledge.name ? String(inworldKnowledge.name) : ''
+    this.knowledgeRecords = inworldKnowledge.knowledgeRecords ? [...inworldKnowledge.knowledgeRecords] : []
+  }
+}
+
+export class UpdateInworldCharacter {
+  name
+  language
+  defaultCharacterDescription
+  defaultCharacterAssets
+  initialMood
+  personality
+  commonKnowledge
+
+  constructor(inworldCharacter = {}) {
+    this.name = String(inworldCharacter.name ?? '')
+    this.language = String(inworldCharacter.language ?? '')
+    this.defaultCharacterDescription = new DefaultCharacterDescription(inworldCharacter.defaultCharacterDescription)
+    this.defaultCharacterAssets = new DefaultCharacterAssets(inworldCharacter.defaultCharacterAssets)
+    this.initialMood = new InitialMood(inworldCharacter.initialMood)
+    this.personality = new Personality(inworldCharacter.personality)
+    this.commonKnowledge = inworldCharacter.commonKnowledge ? [...inworldCharacter.commonKnowledge] : []
+  }
+}
+
+class DefaultCharacterDescription {
+  description
+  pronoun
+  personalityAdjectives
+
+  constructor(defaultCharacterDescription = {}) {
+    this.description = String(defaultCharacterDescription.description ?? '')
+    this.pronoun = String(defaultCharacterDescription.pronoun ?? '')
+    this.personalityAdjectives = defaultCharacterDescription.personalityAdjectives ? [...defaultCharacterDescription.personalityAdjectives] : []
+  }
+}
+
+class DefaultCharacterAssets {
+  voice
+
+  constructor(defaultCharacterAssets = {}) {
+    this.voice = new Voice(defaultCharacterAssets.voice)
+  }
+}
+
+class Voice {
+  baseName
+  pitch
+  speakingRate
+
+  constructor(voice = {}) {
+    this.baseName = String(voice.baseName ?? '')
+    this.pitch = Number(voice.pitch ?? 0)
+    this.speakingRate = Number(voice.speakingRate ?? 0)
+  }
+}
+
+class InitialMood {
+  joy
+  fear
+  trust
+  surprise
+
+  constructor(initialMood = {}) {
+    this.joy = Number(initialMood.joy ?? 0)
+    this.fear = Number(initialMood.fear ?? 0)
+    this.trust = Number(initialMood.trust ?? 0)
+    this.surprise = Number(initialMood.surprise ?? 0)
+  }
+}
+
+class Personality {
+  positive
+  peaceful
+  open
+  extravert
+
+  constructor(personality = {}) {
+    this.positive = Number(personality.positive ?? 0)
+    this.peaceful = Number(personality.peaceful ?? 0)
+    this.open = Number(personality.open ?? 0)
+    this.extravert = Number(personality.extravert ?? 0)
+  }
+}
--- APIs/v1/InworldAPIs/InworldAPIs.js
+++ APIs/v1/InworldAPIs/InworldAPIs.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { InworldCharacter } from './InworldAPIClasses.js'
+import { InworldCharacter, InworldKnowledge, UpdateInworldCharacter } from './InworldAPIClasses.js'
 
 export const inworldv1Router = express.Router()
 
@@ -9,9 +9,21 @@ inworldv1Router.get('/getCharacter', async (req, res) => {
       return res.status(400).json({ response: { error: 'Missing/Incorrect Parameters' } })
 
     const inworldCharacterResponse = await getInworldCharacter(req.query.characterId)
-    return res.status(200).json({ response: { inworldCharacter: inworldCharacterResponse } })
+    return res.status(200).json({ response: inworldCharacterResponse })
   } catch (error) {
     return res.status(500).json({ response: { error: 'Error Getting Character: ' + error.stack } })
+  }
+})
+
+inworldv1Router.put('/updateCharacter', async (req, res) => {
+  try {
+    if (!req.body)
+      return res.status(400).json({ response: { error: 'Missing/Incorrect Parameters' } })
+
+    await updateInworldCharacter(new UpdateInworldCharacter(req.body))
+    return res.status(200).json({ response: 'Updated Character.' })
+  } catch (error) {
+    return res.status(500).json({ response: { error: 'Error Updating Character: ' + error.stack } })
   }
 })
 
@@ -37,9 +49,11 @@ async function getInworldCharacter(characterId) {
       const inworldKnowledgeResponse = await getInworldKnowledge(knowledge)
       characterKnowledge.push(inworldKnowledgeResponse.inworldKnowledge)
     }
-    return { character: inworldCharacterData, knowledge: characterKnowledge }
+
+    inworldCharacterData.commonKnowledge = characterKnowledge
+    return { inworldCharacter: inworldCharacterData }
   } else
-    return { character: {}, knowledge: [] }
+    return { inworldCharacter: {} }
 }
 
 /**
@@ -57,7 +71,79 @@ async function getInworldKnowledge(knowledgeId) {
 
   if (inworldKnowledgeResponse.ok) {
     const inworldKnowledgeData = await inworldKnowledgeResponse.json()
-    return { inworldKnowledge: inworldKnowledgeData }
+    const inworldKnowledge = new InworldKnowledge(inworldKnowledgeData)
+    return { inworldKnowledge: inworldKnowledge }
   } else
-    return { inworldKnowledge: {} }
+    return { inworldKnowledge: [] }
+}
+
+/**
+ * @param {UpdateInworldCharacter} inworldCharacter 
+ */
+async function updateInworldCharacter(inworldCharacter) {
+  const commonKnowledge = inworldCharacter.commonKnowledge
+  delete inworldCharacter.commonKnowledge
+
+  const updateInworldCharacterResponse = await fetch(process.env.INWORLD_API_DOMAIN + inworldCharacter.name, {
+    method: 'PATCH',
+    headers: {
+      'Grpc-Metadata-X-Authorization-Bearer-Type': 'studio_api',
+      'Content-Type': 'application/json',
+      'Authorization': `Basic ${process.env.INWORLD_BASE_64_KEY}`
+    },
+    body: JSON.stringify(
+      inworldCharacter
+    )
+  })
+
+  if (updateInworldCharacterResponse.ok) {
+    const updateInworldCharacterData = await updateInworldCharacterResponse.json()
+    if (!updateInworldCharacterData.message) {
+      await fetch(process.env.INWORLD_API_DOMAIN + inworldCharacter.name + ':deploy', {
+        method: 'POST',
+        headers: {
+          'Grpc-Metadata-X-Authorization-Bearer-Type': 'studio_api',
+          'Content-Type': 'application/json',
+          'Authorization': `Basic ${process.env.INWORLD_BASE_64_KEY}`
+        }
+      })
+
+      for (const _inworldKnowledge of commonKnowledge) {
+        const inworldKnowledge = new InworldKnowledge(_inworldKnowledge)
+        await updateInworldKnowledge(inworldKnowledge)
+      }
+    }
+  }
+}
+
+/**
+ * 
+ * @param {InworldKnowledge} inworldKnowledge 
+ */
+async function updateInworldKnowledge(inworldKnowledge) {
+  const updateInworldKnowledgeResponse = await fetch(process.env.INWORLD_API_DOMAIN + inworldKnowledge.name, {
+    method: 'PATCH',
+    headers: {
+      'Grpc-Metadata-X-Authorization-Bearer-Type': 'studio_api',
+      'Content-Type': 'application/json',
+      'Authorization': `Basic ${process.env.INWORLD_BASE_64_KEY}`
+    },
+    body: JSON.stringify(
+      inworldKnowledge
+    )
+  })
+
+  if (updateInworldKnowledgeResponse.ok) {
+    const updateInworldKnowledgeData = await updateInworldKnowledgeResponse.json()
+    if (!updateInworldKnowledgeData.message) {
+      await fetch(process.env.INWORLD_API_DOMAIN + inworldKnowledge.name + ':deploy', {
+        method: 'POST',
+        headers: {
+          'Grpc-Metadata-X-Authorization-Bearer-Type': 'studio_api',
+          'Content-Type': 'application/json',
+          'Authorization': `Basic ${process.env.INWORLD_BASE_64_KEY}`
+        }
+      })
+    }
+  }
 }
